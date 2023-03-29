bebc6dc6f97f998d657e20315058267c0c48692b
jarod25 <63459117+jarod25@users.noreply.github.com>
2023-03-29 23:45:17 +0200

    Ajout de graphes (ne pas aller sur TOP10, ça crash)

--- appli_web/app.py
+++ appli_web/app.py
@@ -1,29 +1,41 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-from flask import Flask, render_template, redirect, url_for, abort, flash, session, g
-from flask import Blueprint
-import data.data_graph_making as graph
+from flask import Flask, render_template
+from appli_web.data.data_graph_making import graph, graph_elec_consommation, graph_gaz_consommation, graph_eau_consommation, graph_top10
 
 app = Flask(__name__)
 app.secret_key = 'une cle(token) : grain de sel(any random string)'
 
 
-@app.teardown_appcontext
-def close_connection(exception):
-    db = getattr(g, '_database', None)
-    if db is not None:
-        db.close()
-
-
 @app.route('/')
 def show_accueil():
-    return render_template('layout.html')
+    return render_template('home.html')
 
 @app.route('/test')
 def show_test():
-    graph.graph()
-    return render_template('graph.html')
+    graph()
+    return render_template('index.html')
+
+@app.route('/electricite')
+def show_elec():
+    graph_elec_consommation()
+    return render_template('elec.html')
+
+@app.route('/gaz')
+def show_gaz():
+    graph_gaz_consommation()
+    return render_template('gaz.html')
+
+@app.route('/eau')
+def show_eau():
+    graph_eau_consommation()
+    return render_template('eau.html')
+
+@app.route('/top10')
+def show_top10():
+    graph_top10()
+    return render_template('top10.html')
 
 
 if __name__ == '__main__':
--- appli_web/data/data_graph_making.py
+++ appli_web/data/data_graph_making.py
@@ -1,20 +1,53 @@
-from bokeh.plotting import figure
-from bokeh.embed import components
+from bokeh.plotting import figure, output_file, save
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.palettes import Category10_10
 from appli_web.data.data_cleaning import df
 
 
 def graph():
-    consommation_par_annee_filiere = df.groupby(["annee", "filiere"])["consommation"].sum()
+    fig = figure(title='test', x_axis_label='annee', y_axis_label='consommation')
+    fig.line(df['annee'], df['consommation'], line_width=2)
+    output_file('templates/graph.html')
+    save(fig)
 
-    p = figure(x_axis_label="Année", y_axis_label="Consommation", title="Consommation d'électricité, d'eau ou de gaz par année et filière")
-    p.vbar(x=consommation_par_annee_filiere.index, top=consommation_par_annee_filiere.values, width=0.9)
+def graph_elec_consommation():
+    consommation_elec = df[df["filiere"] == "Electricité"].groupby("annee")["consommation"].sum()
+    fig = figure(x_axis_label='annee', y_axis_label='consommation')
+    fig.line(consommation_elec.index, consommation_elec.values, line_width=2)
+    output_file('templates/consommation_elec.html')
+    save(fig)
 
-    script, div = components(p)
+def graph_gaz_consommation():
+    consommation_gaz = df[df["filiere"] == "Gaz"].groupby("annee")["consommation"].sum()
+    fig = figure(x_axis_label='annee', y_axis_label='consommation')
+    fig.line(consommation_gaz.index, consommation_gaz.values, line_width=2)
+    output_file('templates/consommation_gaz.html')
+    save(fig)
 
-    with open("../templates/graph.html", "w") as f:
-        f.write(script)
-        f.write(div)
+def graph_eau_consommation():
+    consommation_eau = df[df["filiere"] == "Eau"].groupby("annee")["consommation"].sum()
+    fig = figure(x_axis_label='annee', y_axis_label='consommation')
+    fig.line(consommation_eau.index, consommation_eau.values, line_width=2)
+    output_file('templates/consommation_eau.html')
+    save(fig)
 
-    print("graph.html created")
-
-graph()
+def graph_top10():
+    # Génère moi un graphique des 10 régions qui consommes le plus au total (tous les années confondues) avec Bokeh
+    # 1. On récupère les 10 régions qui consomment le plus
+    top10 = df.groupby("libelle_region")["consommation"].sum().sort_values(ascending=False).head(10)
+    # 2. On récupère les données de consommation de ces 10 régions
+    top10_data = df[df["libelle_region"].isin(top10.index)]
+    # 3. On crée un graphique avec Bokeh
+    fig = figure(x_axis_label='annee', y_axis_label='consommation', title="Top 10 des régions qui consomment le plus")
+    # 4. On crée une palette de couleur
+    palette = Category10_10
+    # 5. On crée une source de données
+    source = ColumnDataSource(top10_data)
+    # 6. On crée une boucle pour afficher les 10 régions
+    for i, region in enumerate(top10.index):
+        fig.line(x="annee", y="consommation", source=source, color=palette[i], legend_label=region)
+    # 7. On ajoute un outil pour afficher les données au survol de la souris
+    fig.add_tools(HoverTool(tooltips=[("Région", "@libelle_region"), ("Année", "@annee"), ("Consommation", "@consommation")]))
+    # 8. On sauvegarde le graphique
+    output_file('templates/consommation_top10.html')
+    save(fig)
